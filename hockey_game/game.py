3fd9754c5cf4f9aa89478de841070da94b1dd2fd
joselucas77 <jose_lucas77@outlook.com>
2026-05-15 20:28:35 -0300

    Implement Player and Disco classes with movement and drawing logic

--- hockey_game/game.py
+++ hockey_game/game.py
@@ -57,3 +57,108 @@ try:
   player_red = pygame.transForm.scale(player_red, (40,40))
 except:
   player_red = None
+
+# try:
+#   pygame.mixer.music.load("music.mp3")
+# except:
+#   pass
+
+class Player:
+  def __init__(self, x):
+    self.x = x
+    self.y = height // 2 - 60
+    self.width = 20
+    self.height = 120
+    self.speed = 8
+
+  def draw(self):
+    if player_blue:
+      if self.x < width // 2:
+        screen.blit(
+          player_blue,
+          (self.x - 12, self.y - 15)
+        )
+      else:
+        screen.blit(
+          player_red,
+          (self.x - 12, self.y - 15)
+        )
+    else:
+      pygame.draw.rect(
+
+        screen,
+        white,
+        (
+          self.x,
+          self.y,
+          self.width,
+          self.height
+        )
+      )
+
+class Disco:
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.x = width // 2
+    self.y = width // 2
+    self.radius = 15
+    self.speed = 8
+    direction = random.choice([-1, 1])
+    self.speed_x = self.speed * direction
+    self.speed_y = random.uniform(-3, 3)
+    self.fire = False
+    self.plane = False
+
+  def move(self):
+    self.x += self.speed_x
+    self.y += self.speed_y
+    if self.y - self.radius <= 0:
+      self.y = self.radius
+      self.speed_y *= -1
+    if self.y + self.radius >= height:
+      self.y = height - self.radius
+      self.speed_y *= -1
+
+  def draw(self):
+    #se for um avião
+    # if self.plane and plane_img:
+    #   screen.blit(
+    #     plane,
+    #     (self.x - 35, self.y -35)
+    #   )
+    # elif self.fire:
+    # desenho docontorno laranja
+    #   pygame.draw.circle(
+    #     screen,
+    #     orange,
+    #     (int(self.x), int(self.y)),
+    #     self.radius
+    #   )
+    # desenho do centro vermelho
+    # pygame.draw.cricle(
+    #   screen,
+    #   red,
+    #   (int(self.x), int(self.y)),
+    #   self.radius
+    # )
+    # else:
+    if disco_img:
+      screen.blit(
+        disco_img,
+        (self.x - 20, self.y - 20)
+      )
+    else:
+      pygame.draw.circle(
+        screen,
+        white,
+        (int(self.x), int(self.y)),
+        self.radius
+      )
+
+player = [
+  "José Lucas",
+  "Mike",
+  "João"
+]
